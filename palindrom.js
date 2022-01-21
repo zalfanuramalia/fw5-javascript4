c03130d9d8f89e78c2fd0f0e96e6ecd69db281ce
zalfanuramalia <zalfafanur@gmail.com>
2022-01-21 16:08:59 +0700

    file tugas javascript lanjutan

---
+++ palindrom.js
@@ -0,0 +1,14 @@
+function palindrom (kata) {
+    let hasil = "";
+    x = kata.length - 1;
+    for (i=x; i>=0; i--){
+        hasil = hasil + kata[i];
+    }
+    if (hasil === kata){
+        console.log("Palindrom");
+    } else {
+        console.log("Bukan Palindrom");
+    }
+}
+
+palindrom ("kucing")
