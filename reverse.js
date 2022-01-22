762cbbfaa105ed02f75014a4446cf2130f3095dd
zalfanuramalia <zalfafanur@gmail.com>
2022-01-22 08:09:26 +0700

    file tugas javascript lanjutan

--- reverse.js
+++ reverse.js
@@ -1,24 +1,27 @@
 function reverse (text){
-    if (text == "string"){
-        let hasil ="";
-        let find = "";
-        x = text.length - 1;
-        for (i = 0; i <= x; i++){
-            find = find + text[i];
-            if ( text[i]==" "){
+    if (typeof text == "string"){
+        let hasil="";
+        let hasil2="";
+        let find= "";
+        x=0;
+        while (x <= text.length){
+            if (text[x]){
+                find = find + text[x];
+            }
+            if ( text[x]==" "){
                 hasil = find + hasil;
                 find ="";
             }
             if (x==text.length){
-                hasil = find + " " + hasil;
+                hasil2 = find + " " + hasil;
                 find="";
             }
-            console.log(hasil)
+            x++;
         }
+        console.log(hasil2);
     } else {
         console.log("input teks salah")
     }
-    console.log(typeof text);
 }
 
 reverse("Saya belajar Javascript")
